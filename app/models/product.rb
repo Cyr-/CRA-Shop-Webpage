2aee68292e176c3a59118513dadc39b08e7bb774
TJ <Cyr-@users.noreply.github.com>
2014-11-10 21:24:25 -0600

    Creating products view with Slim

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :name, :price, :stock_quantity, :description, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :stock_quantity, numericality: { only_integer: true }
 end
