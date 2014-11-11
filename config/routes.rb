5a640721dad44333f082699b10f469aac344be06
TJ <Cyr-@users.noreply.github.com>
2014-11-11 16:13:48 -0600

    Added an About page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
+
   root 'products#index'
 
   get 'products' => 'products#products'
 
   get 'products/:id' => 'products#show', as: 'product'
+
+  get 'about' => 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
