69bdbd2246399f5275663bf3fd554f4563210985
TJ <Cyr-@users.noreply.github.com>
2014-11-11 12:26:51 -0600

    Created products and show views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,19 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
-    @products = Product.order("id").page(params[:page]).per(5)
+    @products = Product.order('id').limit(6)
+  end
+
+  def products
+    @products = Product.order('id').page(params[:page]).per(5)
+  end
+
+  def show
   end
 end
+
+private
+  def set_product
+    @product = Product.find(params[:id])
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root 'products#index'
 
+  get 'products' => 'products#products'
+
+  get 'products/:id' => 'products#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
