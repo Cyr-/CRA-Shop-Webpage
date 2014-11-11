40061ab2449cac061b24955d29547db832347b30
TJ <Cyr-@users.noreply.github.com>
2014-11-11 13:40:29 -0600

    Repaired links to product pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'products' => 'products#products'
 
-  get 'products/:id' => 'products#show'
+  get 'products/:id' => 'products#show', as: 'product'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
