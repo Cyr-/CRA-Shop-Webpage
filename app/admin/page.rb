fb170e8cb46c8dc431790d451bfe3a0279fd1cd1
TJ <Cyr-@users.noreply.github.com>
2014-11-12 19:24:22 -0600

    Added Page model to handle About and Contact pages

---
+++ app/admin/page.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Page do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  menu priority: 4
+
+  permit_params :title, :content
+
+  index :title => "Pages" do
+    column :title
+
+    column :content
+
+    actions
+  end
+
+end
