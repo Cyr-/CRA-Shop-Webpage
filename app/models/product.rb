c3fb759b019c3bf61e36c402bd38d5a89812ccc3
TJ <Cyr-@users.noreply.github.com>
2014-11-17 19:34:59 -0600

    Added image uploader to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ActiveRecord::Base
   validates :name, :price, :stock_quantity, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :stock_quantity, numericality: { only_integer: true }
+
+  mount_uploader :image, ImageUploader
 end
