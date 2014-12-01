3f16fae2eb1d23de6436d3f4bfe615bc520d5dc3
TJ <Cyr-@users.noreply.github.com>
2014-11-30 20:47:44 -0600

    Updating associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   belongs_to :category
+  has_many :line_items
 
   validates :name, :price, :stock_quantity,
             :description, :sale_price, presence: true
