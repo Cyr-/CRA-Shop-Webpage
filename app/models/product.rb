f9abc2bf22ec7a4e9068bd4be4aab96a393a847f
TJ <Cyr-@users.noreply.github.com>
2014-12-09 15:10:16 -0600

    Minor modifications

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   belongs_to :category
-  has_many :orders, :through => :line_items
-  
+  has_many :orders, as: :line_items
+
   validates :name, :price, :stock_quantity,
             :description, :sale_price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
